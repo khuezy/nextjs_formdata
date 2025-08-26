b407a2dc48fba00483db4c66508be36ca091fd4a
Khue Nguyen <khuezy.nguyen@gmail.com>
2025-08-26 16:08:56 -0700

    log file

--- app/action.ts
+++ app/action.ts
@@ -3,6 +3,7 @@
 export async function serverActionUpload(fd: FormData) {
   const file = fd.get('file') as unknown as File
 
+  console.log('file:', file)
   if (!file) {
     throw new Error('File is missing!')
   }
