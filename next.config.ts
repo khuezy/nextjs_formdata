aa06845c9f52da520b0a9e46db7b7632ceb3a1b5
Khue Nguyen <khuezy.nguyen@gmail.com>
2025-08-26 16:00:07 -0700

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  experimental: {
+    preloadEntriesOnStart: false,
+    webpackMemoryOptimizations: true,
+    middlewarePrefetch: 'strict',
+    reactCompiler: true,
+    ppr: true,
+    cacheComponents: true,
+    serverMinification: true,
+    nextScriptWorkers: true,
+  }
 };
 
 export default nextConfig;
